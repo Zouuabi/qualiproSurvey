868a19e34d94254cc10204ab37837f8574ee1bee
zouuabi <zouabifoued8@gmail.com>
2024-08-12 20:38:11 +0100

    introduction page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 
 import { SurveyComponent } from './survey/survey.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 export const routes: Routes = [
 
     {
         path:'',
+        component : IntroductionComponent,
+        title : 'Itroduction'
+
+    },
+    {
+        path:'survey',
         component : SurveyComponent,
         title : 'Survey'
 
